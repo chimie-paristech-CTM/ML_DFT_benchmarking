1918cfeb577cd8fa36570a30743df391147e7d58
Javier Emilio Alfonso Ramos <javialra97@gmail.com>
2024-04-01 16:11:18 +0200

    Chemical space

--- scripts/generate_space.py
+++ scripts/generate_space.py
@@ -1,78 +1,31 @@
 import pandas as pd
-from rdkit import Chem
-import itertools
-import re
-import numpy as np
-
-# substituent list
-subs_list_LR = ['C', 'F', 'OC', 'O', 'C#N', 'C(=O)OC', 'OC(=O)C', 'NC', 'SC', None]
-
-# make easy the replacement
-labels = ['[Os]', '[Ir]', '[Pt]', '[Au]']
-
-
-def single_edit_mol(mol, label, subs):
-    if subs != None:
-        mod_mol = Chem.ReplaceSubstructs(mol, Chem.MolFromSmiles(label), Chem.MolFromSmiles(subs), replaceAll=True)[0]
-    else:
-        mod_mol = Chem.DeleteSubstructs(mol, Chem.MolFromSmiles(label))
-    return mod_mol
-
-
-def modify_mol(dipole, subs_comb_LR, labels):
-    mol = Chem.MolFromSmiles(dipole)
-    for i, subs in enumerate(subs_comb_LR):
-        mol = single_edit_mol(mol, labels[i], subs)
-
-    return Chem.MolFromSmiles(Chem.MolToSmiles(mol))
-
-
-def unmap_smiles(smiles):
-    """Unmap atoms of SMILES"""
-    mol = Chem.MolFromSmiles(smiles)
-    [atom.SetAtomMapNum(0) for atom in mol.GetAtoms()]
-
-    return Chem.MolToSmiles(mol)
-
-
-def create_combination(rxn_smiles, type):
-
-    reacs, prods = rxn_smiles.split(">>")
-
-    r_groups = 0
-    for label in labels:
-        if label in reacs:
-            r_groups += 1
-
-    substituent_combs = itertools.product(subs_list_LR, repeat=r_groups)
-
-    new_reacs, new_prods = [], []
-    for subs_comb in substituent_combs:
-        if r_groups > 2:
-            if len(set(subs_comb)) == 1:
-                continue
-            if subs_comb.count(None) < r_groups - 2:   # to avoid a combinatorial explosion
-                continue
-        new_reacs.append(modify_mol(reacs, subs_comb, labels))
-        new_prods.append(modify_mol(prods, subs_comb, labels))
-
-    new_rxns = []
-    for reac, prod in zip(new_reacs, new_prods):
-        reac_smi = Chem.MolToSmiles(reac)
-        prod_smi = Chem.MolToSmiles(prod)
-        new_rxns.append(f"{reac_smi}>>{prod_smi}")
-
-    df_rxns = pd.DataFrame()
-    df_rxns['Type'] = len(new_rxns) * [type]
-    df_rxns['rxn_smiles'] = new_rxns
-
-    return df_rxns
+from lib.chemical_space import create_combination, bicyclic_core, tricyclic_core
 
 
 if __name__ == '__main__':
 
-    df = pd.read_csv('../hypothetical_space_core.csv', sep=';')
+    bi_cores_list = [('Diels-Alder','C=CMgC([Os])=CC=C[Os]>>C1MgC([Os])2C=CCC([Os])1'),
+                     ('[3+2]cycloaddition','[Os]C=CMgC([Ir])=[N+]([O-])\C>>[Os][C@@H]1CMg[C@@]2([Ir])N(C)O1'),
+                     ('[3+2]cycloaddition','[Os]C(Mg/C(C(C)C)=[N+]([O-])\[Ir])=C=O>>[Os]CMg=C1O[C@]2(C(C)C)N([Ir])O1'),
+                     ('[3+2]cycloaddition','[Os]/C(MgC([Ir])=C/[Ir])=[N+]1N[C@H]2CC[C@@H]/1C2>>[Os][C@@]1MgN3[NH+]([C@@H]([C@]12[Ir])[Ir])[C@H]4CC[C@@H]3C4')]
+    tri_cores_list = ['C([Os])1=CC=C([Os])MgC=CMg1>>C([Os])12C=CC([Os])3MgC3C1Mg2',]
+
+    rxns = []
+    for sub in ['C', 'C([Os])C', 'CC([Os])', 'C(=O)C(=O)', 'CC(=O)O', 'C([Os])CCCC', 'C([Os])CCCCC']:
+        rxn = tricyclic_core(tri_cores_list[0], sub)
+        rxns.append(('Diels-Alder', rxn))
+        for bi_core in bi_cores_list:
+            rxn = bicyclic_core(bi_core[1], sub)
+            rxns.append((bi_core[0], rxn))
+
+    rxns = list(set(rxns))
+    df_fused_cycles = pd.DataFrame(rxns, columns=['Type', 'rxn_smiles'])
+
+    df = pd.read_csv('../data/hypothetical_space_core.csv', sep=';')
+
+    df = pd.concat([df, df_fused_cycles], ignore_index=True, axis=0)
+
     df_space = pd.DataFrame(columns=['Type', 'rxn_smiles'])
     for row in df.itertuples():
         df_space = pd.concat([df_space, create_combination(row.rxn_smiles, row.Type)], axis=0, ignore_index=True)
-    df_space.to_csv('../hypothetical_chemical_space.csv')
+    df_space.to_csv('hypothetical_chemical_space.csv')
--- scripts/lib/chemical_space.py
+++ scripts/lib/chemical_space.py
@@ -1,10 +1,81 @@
 import pandas as pd
 from rdkit import Chem
-import re
+import itertools
 
 
-def bicyclic_core(reac, prod, sub):
+# Functions to decorate the cores
 
+
+# substituent list
+subs_list_LR = ['C=O', 'O', 'C#N', 'C(=O)OC', 'OC(=O)C', 'C(C)(C)C', None]
+
+# make easy the replacement
+labels = ['[Os]', '[Ir]', '[Pt]', '[Au]']
+
+
+def single_edit_mol(mol, label, subs):
+    if subs != None:
+        mod_mol = Chem.ReplaceSubstructs(mol, Chem.MolFromSmiles(label), Chem.MolFromSmiles(subs), replaceAll=True)[0]
+    else:
+        mod_mol = Chem.DeleteSubstructs(mol, Chem.MolFromSmiles(label))
+    return mod_mol
+
+
+def modify_mol(dipole, subs_comb_LR, labels):
+    mol = Chem.MolFromSmiles(dipole)
+    for i, subs in enumerate(subs_comb_LR):
+        mol = single_edit_mol(mol, labels[i], subs)
+
+    return Chem.MolFromSmiles(Chem.MolToSmiles(mol))
+
+
+def unmap_smiles(smiles):
+    """Unmap atoms of SMILES"""
+    mol = Chem.MolFromSmiles(smiles)
+    [atom.SetAtomMapNum(0) for atom in mol.GetAtoms()]
+
+    return Chem.MolToSmiles(mol)
+
+
+def create_combination(rxn_smiles, type):
+
+    reacs, prods = rxn_smiles.split(">>")
+
+    r_groups = 0
+    for label in labels:
+        if label in reacs:
+            r_groups += 1
+
+    substituent_combs = itertools.product(subs_list_LR, repeat=r_groups)
+
+    new_reacs, new_prods = [], []
+    for subs_comb in substituent_combs:
+        if r_groups > 2:
+            if len(set(subs_comb)) == 1:
+                continue
+            if subs_comb.count(None) < r_groups - 2:   # to avoid a combinatorial explosion
+                continue
+        new_reacs.append(modify_mol(reacs, subs_comb, labels))
+        new_prods.append(modify_mol(prods, subs_comb, labels))
+
+    new_rxns = []
+    for reac, prod in zip(new_reacs, new_prods):
+        reac_smi = Chem.MolToSmiles(reac)
+        prod_smi = Chem.MolToSmiles(prod)
+        new_rxns.append(f"{reac_smi}>>{prod_smi}")
+
+    df_rxns = pd.DataFrame()
+    df_rxns['Type'] = len(new_rxns) * [type]
+    df_rxns['rxn_smiles'] = new_rxns
+
+    return df_rxns
+
+# Functions to generate fused cycles
+
+
+def bicyclic_core(rxn, sub):
+
+    reac, prod = rxn.split('>>')
     reac = reac.replace('Mg', sub)
     prod = prod.replace('Mg', f"({sub}2)")
     mol_reac = Chem.MolFromSmiles(reac)
@@ -18,8 +89,9 @@ def bicyclic_core(reac, prod, sub):
     return rxn
 
 
-def tricyclic_core(reac, prod, sub):
+def tricyclic_core(rxn, sub):
 
+    reac, prod = rxn.split('>>')
     reac = reac.replace('Mg', sub)
     prod = prod.replace('Mg', f"{sub}")
     mol_reac = Chem.MolFromSmiles(reac)
