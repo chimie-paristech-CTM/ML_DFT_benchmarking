a5f582f09817729544adcbc26a8213e8c9635c37
Javier Emilio Alfonso Ramos <javialra97@gmail.com>
2024-04-03 17:47:50 +0200

    Nested cross-validation for fingerprints

--- scripts/baseline_models.py
+++ scripts/baseline_models.py
@@ -9,7 +9,12 @@ from lib.final_functions import get_cross_val_accuracy_rf, get_cross_val_accurac
 from lib.final_functions import get_optimal_parameters_xgboost, get_optimal_parameters_xgboost_fp
 from lib.final_functions import get_cross_val_accuracy_xgboost, get_cross_val_accuracy_xgboost_fps
 from lib.fingerprints import get_fingerprints_DRFP, get_fingerprints_Morgan
-
+from lib.nested_cross_val import nested_cross_val_fp
+from lib.bayesian_opt import objective_knn_fp, objective_rf_fp, objective_xgboost_fp
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+from hyperopt import hp
 
 
 parser = ArgumentParser()
@@ -41,18 +46,37 @@ if __name__ == '__main__':
 
     # one-hot encoding
     # KNN
-    optimal_parameters_knn = get_optimal_parameters_knn(df, logger, max_eval=64)
-    get_cross_val_accuracy_knn(df, logger, n_fold, optimal_parameters_knn, split_dir)
+    #optimal_parameters_knn = get_optimal_parameters_knn(df, logger, max_eval=64)
+    #get_cross_val_accuracy_knn(df, logger, n_fold, optimal_parameters_knn, split_dir)
 
     ## RF
-    optimal_parameters_rf = get_optimal_parameters_rf(df, logger, max_eval=64)
-    get_cross_val_accuracy_rf(df, logger, n_fold, optimal_parameters_rf, split_dir)
+    # optimal_parameters_rf = get_optimal_parameters_rf(df, logger, max_eval=64)
+    # get_cross_val_accuracy_rf(df, logger, n_fold, optimal_parameters_rf, split_dir)
+    #
+    # ## XGboost
+    # optimal_parameters_xgboost = get_optimal_parameters_xgboost(df, logger, max_eval=128)
+    # get_cross_val_accuracy_xgboost(df, logger, n_fold, optimal_parameters_xgboost, split_dir)
+    #
 
-    ## XGboost
-    optimal_parameters_xgboost = get_optimal_parameters_xgboost(df, logger, max_eval=128)
-    get_cross_val_accuracy_xgboost(df, logger, n_fold, optimal_parameters_xgboost, split_dir)
+    space_knn = {
+        'n_neighbors': hp.quniform('n_neighbors', low=3, high=15, q=2),
+    }
+    space_rf = {
+        'n_estimators': hp.choice('n_estimators', [10, 30, 50, 100, 150, 200, 300, 400, 600]),
+        'max_features': hp.quniform('max_features', low=0.1, high=1, q=0.1),
+        'min_samples_leaf': hp.choice('min_samples_leaf', [1, 2, 5, 10, 20, 50])
+    }
+    space_xgb = {
+        'max_depth': hp.quniform('max_depth', low=2, high=10, q=1),
+        'gamma': hp.qloguniform('gamma', low=0.0, high=6.0, q=2.0),
+        'n_estimators': hp.quniform('n_estimators', low=100, high=800, q=100),
+        'learning_rate': hp.quniform('learning_rate', low=0.05, high=0.20, q=0.05),
+        'min_child_weight': hp.quniform('min_child_weight', low=2, high=10, q=2.0)
+    }
 
-    # fingerprints
+
+
+    # # fingerprints
     logger.info(f"Fingerprints")
     nbits = [16, 32, 64, 128, 256, 512, 1024, 2048]
     rads = [1, 2, 3]
@@ -63,29 +87,52 @@ if __name__ == '__main__':
 
             # KNN fingerprints
             logger.info(f"Fingerprint: DRFP (radius={rad}, nbits={nbit})")
-            optimal_parameters_knn_fps = get_optimal_parameters_knn_fp(df_fps_drfp, logger, max_eval=64)
-            get_cross_val_accuracy_knn_fp(df_fps_drfp, logger, n_fold, optimal_parameters_knn_fps, split_dir)
+            #optimal_parameters_knn_fps = get_optimal_parameters_knn_fp(df_fps_drfp, logger, max_eval=64)
+            #get_cross_val_accuracy_knn_fp(df_fps_drfp, logger, n_fold, optimal_parameters_knn_fps, split_dir)
+            rmse, mae, r2 = nested_cross_val_fp(df_fps_drfp, 6, space=space_knn, objective=objective_knn_fp,
+                                                model_class=KNeighborsRegressor,
+                                                max_eval=32, logger=logger)
+            logger.info(f'6-fold nested-cv RMSE, MAE and R^2 for KNN fps {rmse:.3f} {mae:.3f} {r2:.3f}')
 
             logger.info(f"Fingerprint: Morgan (radius={rad}, nbits={nbit})")
-            optimal_parameters_knn_fps = get_optimal_parameters_knn_fp(df_fps_morgan, logger, max_eval=64)
-            get_cross_val_accuracy_knn_fp(df_fps_morgan, logger, n_fold, optimal_parameters_knn_fps, split_dir)
+            #optimal_parameters_knn_fps = get_optimal_parameters_knn_fp(df_fps_morgan, logger, max_eval=64)
+            #get_cross_val_accuracy_knn_fp(df_fps_morgan, logger, n_fold, optimal_parameters_knn_fps, split_dir)
+            rmse, mae, r2 = nested_cross_val_fp(df_fps_morgan, 6, space=space_knn, objective=objective_knn_fp,
+                                                model_class=KNeighborsRegressor,
+                                                max_eval=32, logger=logger)
+            logger.info(f'6-fold nested-cv RMSE, MAE and R^2 for KNN fps {rmse:.3f} {mae:.3f} {r2:.3f}')
 
             # RF fingerprints
             logger.info(f"Fingerprint: DRFP (radius={rad}, nbits={nbit})")
-            optimal_parameters_rf_fps = get_optimal_parameters_rf_fp(df_fps_drfp, logger, max_eval=64)
-            get_cross_val_accuracy_rf_fps(df_fps_drfp, logger, n_fold, optimal_parameters_rf_fps, split_dir)
+            #optimal_parameters_rf_fps = get_optimal_parameters_rf_fp(df_fps_drfp, logger, max_eval=64)
+            #get_cross_val_accuracy_rf_fps(df_fps_drfp, logger, n_fold, optimal_parameters_rf_fps, split_dir)
+            rmse, mae, r2 = nested_cross_val_fp(df_fps_drfp, 6, space=space_rf, objective=objective_rf_fp,
+                                               model_class=RandomForestRegressor,
+                                               max_eval=64, logger=logger)
+            logger.info(f'6-fold nested-cv RMSE, MAE and R^2 for RF fps {rmse:.3f} {mae:.3f} {r2:.3f}')
 
             logger.info(f"Fingerprint: Morgan (radius={rad}, nbits={nbit})")
-            optimal_parameters_rf_fps = get_optimal_parameters_rf_fp(df_fps_morgan, logger, max_eval=64)
-            get_cross_val_accuracy_rf_fps(df_fps_morgan, logger, n_fold, optimal_parameters_rf_fps, split_dir)
+            #optimal_parameters_rf_fps = get_optimal_parameters_rf_fp(df_fps_morgan, logger, max_eval=64)
+            #get_cross_val_accuracy_rf_fps(df_fps_morgan, logger, n_fold, optimal_parameters_rf_fps, split_dir)
+            rmse, mae, r2 = nested_cross_val_fp(df_fps_morgan, 6, space=space_rf, objective=objective_rf_fp,
+                                                model_class=RandomForestRegressor,
+                                                max_eval=64, logger=logger)
+            logger.info(f'6-fold nested-cv RMSE, MAE and R^2 for RF fps {rmse:.3f} {mae:.3f} {r2:.3f}')
 
             # XGboost fingerprints
             logger.info(f"Fingerprint: DRFP (radius={rad}, nbits={nbit})")
-            optimal_parameters_xgboost_fp = get_optimal_parameters_xgboost_fp(df_fps_drfp, logger, max_eval=128)
-            get_cross_val_accuracy_xgboost_fps(df_fps_drfp, logger, n_fold, optimal_parameters_xgboost_fp, split_dir)
+            #optimal_parameters_xgboost_fp = get_optimal_parameters_xgboost_fp(df_fps_drfp, logger, max_eval=128)
+            #get_cross_val_accuracy_xgboost_fps(df_fps_drfp, logger, n_fold, optimal_parameters_xgboost_fp, split_dir)
+            rmse, mae, r2 = nested_cross_val_fp(df_fps_drfp, 6, space=space_xgb, objective=objective_xgboost_fp,
+                                                model_class=XGBRegressor,
+                                                max_eval=128, logger=logger)
+            logger.info(f'6-fold nested-cv RMSE, MAE and R^2 for XGBoost fps {rmse:.3f} {mae:.3f} {r2:.3f}')
 
             logger.info(f"Fingerprint: Morgan (radius={rad}, nbits={nbit})")
-            optimal_parameters_xgboost_fp = get_optimal_parameters_xgboost_fp(df_fps_morgan, logger, max_eval=128)
-            get_cross_val_accuracy_xgboost_fps(df_fps_morgan, logger, n_fold, optimal_parameters_xgboost_fp, split_dir)
-
+            #optimal_parameters_xgboost_fp = get_optimal_parameters_xgboost_fp(df_fps_morgan, logger, max_eval=128)
+            #get_cross_val_accuracy_xgboost_fps(df_fps_morgan, logger, n_fold, optimal_parameters_xgboost_fp, split_dir)
+            rmse, mae, r2 = nested_cross_val_fp(df_fps_morgan, 6, space=space_xgb, objective=objective_xgboost_fp,
+                                                model_class=XGBRegressor,
+                                                max_eval=128, logger=logger)
+            logger.info(f'6-fold nested-cv RMSE, MAE and R^2 for XGBoost fps {rmse:.3f} {mae:.3f} {r2:.3f}')
 
---
+++ scripts/lib/nested_cross_val.py
@@ -0,0 +1,192 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.preprocessing import StandardScaler
+import re
+from lib.bayesian_opt import bayesian_opt
+from lib.cross_val import one_hot_encoding
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.neighbors import KNeighborsRegressor
+
+n_estimator_dict = {0: 10, 1: 30, 2: 50, 3: 100, 4: 150, 5: 200, 6: 300, 7: 400, 8: 600}
+min_samples_leaf_dict = {0: 1, 1: 2, 2: 5, 3: 10, 4: 20, 5: 50}
+
+
+
+def nested_cross_val(df, n_folds, space, objective, model_class, max_eval, logger,
+                     target_column='Std_DFT_forward', sample=None, split_dir=None):
+    """
+    Function to perform nested cross-validation
+
+    Args:
+        df (pd.DataFrame): the DataFrame containing features and targets
+        model (sklearn.Regressor): An initialized sklearn model
+        n_folds (int): the number of folds
+        target_column (str): target column
+        sample(int): the size of the subsample for the training set (default = None)
+        split_dir (str): the path to a directory containing data splits. If None, random splitting is performed.
+
+    Returns:
+        int: the obtained RMSE and MAE
+    """
+    rmse_list, mae_list, r2_list = [], [], []
+    df, encode_columns = one_hot_encoding(df)
+
+    if split_dir == None:
+        df = df.sample(frac=1, random_state=0)
+        chunk_list = np.array_split(df, n_folds)
+
+    for i in range(n_folds):
+        if split_dir == None:
+            df_train = pd.concat([chunk_list[j] for j in range(n_folds) if j != i])
+            if sample != None:
+                df_train = df_train.sample(n=sample)
+            df_test = chunk_list[i]
+        else:
+            rxn_ids_train1 = pd.read_csv(os.path.join(split_dir, f'fold_{i}/train.csv'))[['rxn_id']].values.tolist()
+            rxn_ids_train2 = pd.read_csv(os.path.join(split_dir, f'fold_{i}/valid.csv'))[['rxn_id']].values.tolist()
+            rxn_ids_train = list(np.array(rxn_ids_train1 + rxn_ids_train2).reshape(-1))
+            df['train'] = df['rxn_id'].apply(lambda x: int(x) in rxn_ids_train)
+            df_train = df[df['train'] == True]
+            df_test = df[df['train'] == False]
+
+        X_train, y_train = df_train[encode_columns], df_train[[target_column]]
+        X_test, y_test = df_test[encode_columns], df_test[[target_column]]
+
+        optimal_parameters = bayesian_opt(df_train, space, objective, model_class, max_eval=max_eval)
+        match model_class.__name__:
+            case 'RandomForestRegressor':
+                model = RandomForestRegressor(n_estimators=int(optimal_parameters['n_estimators']),
+                                              max_features=optimal_parameters['max_features'],
+                                              min_samples_leaf=int(optimal_parameters['min_samples_leaf']))
+            case 'KNeighborsRegressor':
+                model = KNeighborsRegressor(n_neighbors=int(optimal_parameters['n_neighbors']), weights='distance')
+            case 'XGBRegressor':
+                model = XGBRegressor(max_depth=int(optimal_parameters['max_depth']),
+                                     gamma=optimal_parameters['gamma'],
+                                     n_estimators=int(optimal_parameters['n_estimators']),
+                                     learning_rate=optimal_parameters['learning_rate'],
+                                     min_child_weight=optimal_parameters['min_child_weight'])
+
+        logger.info(f'Optimal parameters for {model_class.__name__} -- one-hot-encoding: {optimal_parameters}')
+
+        target_scaler = StandardScaler()
+        target_scaler.fit(y_train)
+        y_train = target_scaler.transform(y_train)
+        y_test = target_scaler.transform(y_test)
+
+        # fit and compute rmse and mae
+        model.fit(X_train, y_train.ravel())
+        predictions = model.predict(X_test)
+        predictions = predictions.reshape(-1, 1)
+
+        rmse_fold = np.sqrt(
+            mean_squared_error(target_scaler.inverse_transform(predictions), target_scaler.inverse_transform(y_test)))
+        rmse_list.append(rmse_fold)
+
+        mae_fold = mean_absolute_error(target_scaler.inverse_transform(predictions),
+                                       target_scaler.inverse_transform(y_test))
+        mae_list.append(mae_fold)
+
+        r2_fold = r2_score(target_scaler.inverse_transform(y_test), target_scaler.inverse_transform(predictions))
+        r2_list.append(r2_fold)
+
+    rmse = np.mean(np.array(rmse_list))
+    mae = np.mean(np.array(mae_list))
+    r2 = np.mean(np.array(r2_list))
+
+    return rmse, mae, r2
+
+
+def nested_cross_val_fp(df_fp, n_folds, space, objective, model_class, max_eval, logger,
+                        target_column='Std_DFT_forward', split_dir=None):
+    """
+    Function to perform cross-validation with fingerprints
+
+    Args:
+        df_fp (pd.DataFrame): the DataFrame containing fingerprints and targets
+        model (sklearn.Regressor): An initialized sklearn model
+        n_folds (int): the number of folds
+        target_column (str): target column
+        split_dir (str): the path to a directory containing data splits. If None, random splitting is performed.
+
+    Returns:
+        int: the obtained RMSE
+    """
+    rmse_list, mae_list, r2_list = [], [], []
+
+    if split_dir == None:
+        df_fp = df_fp.sample(frac=1, random_state=0)
+        chunk_list = np.array_split(df_fp, n_folds)
+
+    for i in range(n_folds):
+        if split_dir == None:
+            df_train = pd.concat([chunk_list[j] for j in range(n_folds) if j != i])
+            df_test = chunk_list[i]
+        else:
+            rxn_ids_train1 = pd.read_csv(os.path.join(split_dir, f'fold_{i}/train.csv'))[['rxn_id']].values.tolist()
+            rxn_ids_train2 = pd.read_csv(os.path.join(split_dir, f'fold_{i}/valid.csv'))[['rxn_id']].values.tolist()
+            rxn_ids_train = list(np.array(rxn_ids_train1 + rxn_ids_train2).reshape(-1))
+            df_fp['train'] = df_fp['rxn_id'].apply(lambda x: int(x) in rxn_ids_train)
+            df_train = df_fp[df_fp['train'] == True]
+            df_test = df_fp[df_fp['train'] == False]
+
+        y_train = df_train[[target_column]]
+        y_test = df_test[[target_column]]
+
+        optimal_parameters = bayesian_opt(df_train, space, objective, model_class, max_eval=max_eval)
+        match model_class.__name__:
+            case 'RandomForestRegressor':
+                optimal_parameters['n_estimators'] = n_estimator_dict[optimal_parameters['n_estimators']]
+                optimal_parameters['min_samples_leaf'] = min_samples_leaf_dict[optimal_parameters['min_samples_leaf']]
+                model = RandomForestRegressor(n_estimators=int(optimal_parameters['n_estimators']),
+                                              max_features=optimal_parameters['max_features'],
+                                              min_samples_leaf=int(optimal_parameters['min_samples_leaf']))
+            case 'KNeighborsRegressor':
+                model = KNeighborsRegressor(n_neighbors=int(optimal_parameters['n_neighbors']), weights='distance')
+            case 'XGBRegressor':
+                model = XGBRegressor(max_depth=int(optimal_parameters['max_depth']),
+                                     gamma=optimal_parameters['gamma'],
+                                     n_estimators=int(optimal_parameters['n_estimators']),
+                                     learning_rate=optimal_parameters['learning_rate'],
+                                     min_child_weight=optimal_parameters['min_child_weight'])
+
+        #logger.info(f'Optimal parameters for {model_class.__name__} {i} fold: {optimal_parameters}')
+
+        # scale targets
+        target_scaler = StandardScaler()
+        target_scaler.fit(y_train)
+        y_train = target_scaler.transform(y_train)
+        y_test = target_scaler.transform(y_test)
+
+        X_train = []
+        for fp in df_train['Fingerprints'].values.tolist():
+            X_train.append(list(fp))
+        X_test = []
+        for fp in df_test['Fingerprints'].values.tolist():
+            X_test.append(list(fp))
+
+        # fit and compute rmse
+        model.fit(X_train, y_train.ravel())
+        predictions = model.predict(X_test)
+        predictions = predictions.reshape(-1, 1)
+
+        rmse_fold = np.sqrt(
+            mean_squared_error(target_scaler.inverse_transform(predictions), target_scaler.inverse_transform(y_test)))
+        rmse_list.append(rmse_fold)
+
+        mae_fold = mean_absolute_error(target_scaler.inverse_transform(predictions),
+                                       target_scaler.inverse_transform(y_test))
+        mae_list.append(mae_fold)
+
+        r2_fold = r2_score(target_scaler.inverse_transform(y_test), target_scaler.inverse_transform(predictions))
+        r2_list.append(r2_fold)
+
+    rmse = np.mean(np.array(rmse_list))
+    mae = np.mean(np.array(mae_list))
+    r2 = np.mean(np.array(r2_list))
+
+    return rmse, mae, r2
+
