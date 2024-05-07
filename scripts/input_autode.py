0b190e080da4e4542ed49c85dadd1d2960a1bf6f
Javier Emilio Alfonso Ramos <javialra97@gmail.com>
2024-05-07 15:26:07 +0200

    change level of theory

--- scripts/input_autode.py
+++ scripts/input_autode.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
-
 import os
 import pandas as pd
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument('--csv_file', type=str, default='../data/rxns_benchmarked.csv',
-                    help='path to csv file containing reactions')
 parser.add_argument('--final_dir', type=str, default='../data/autodE_input',
                     help='path to the folder with the autodE input')
 parser.add_argument('--conda_env', type=str, default='autodE',
                     help='conda environment of autodE package')
+parser.add_argument('--input', type=str, default=None,
+                    help='path to txt file containing reactions')
 
 
 def compare_workflow():
@@ -19,19 +18,20 @@ def compare_workflow():
     rxn_benchmarked = df.sample(n=7, random_state=7) # 5%
     rxn_benchmarked.to_csv('../data/rxns_benchmarked.csv')
 
-def create_input_autodE(final_dir, csv_file, conda_env="autodE"):
+def create_input_autodE(final_dir, input_file, conda_env="autodE"):
 
-    create_input(csv_file, final_dir, conda_env)
+    create_input(input_file, final_dir, conda_env)
 
     return None
 
 
-def create_input(csv_file, final_dir, conda_env, hmet_confor=r"True"):
+def create_input(input_file, final_dir, conda_env, hmet_confor=r"True"):
     """ Create the input for autoDE"""
-    
-    current_dir = os.getcwd()
 
-    rxns = pd.read_csv(csv_file, index_col=0)
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+
+    current_dir = os.getcwd()
 
     # checking directory
     if os.path.isdir(final_dir):
@@ -40,12 +40,11 @@ def create_input(csv_file, final_dir, conda_env, hmet_confor=r"True"):
         os.mkdir(final_dir)
         os.chdir(final_dir)
 
-    for rxn in rxns.itertuples():
+    for line in lines:
 
-        rxn_smile = rxn.rxn_smiles
-        idx = rxn.Index + 1
+        idx, rxn_smile = line.split()
 
-        directory = f"rxn_{idx:04}"
+        directory = f"rxn_{idx}"
         os.mkdir(directory)
 
         create_ade_input(rxn_smile, idx, directory, hmet_confor=hmet_confor)
@@ -65,38 +64,34 @@ def create_ade_input(rxn_smile, idx, dir, hmet_confor=r"True"):
     # Setting the calculation
 
     functional = 'cam-b3lyp'
-    final_basis_set = '6-311++G**'
-    initial_basis_set = '6-31G*'
+    conf_basis_set = '6-31G*'
+    basis_set = '6-311++G**'
     cores = 24
     mem = 4000
     num_conf = 1000
-    rmsd = 0.1
+    rmsd = 0.3
 
-    file_name = f"ade_{idx:05}.py"
+    file_name = f"ade_{idx}.py"
 
     with open(f"{dir}/{file_name}", 'w') as in_ade:
         in_ade.write('import autode as ade\n')
-        in_ade.write('import time\n')
-        in_ade.write('t0 = time.time()\n')
         in_ade.write(f"ade.Config.n_cores={cores}\n")
         in_ade.write(f"ade.Config.max_core={mem}\n")
         in_ade.write(f"ade.Config.hcode=\"G16\"\n")
         in_ade.write(f"ade.Config.lcode =\"xtb\"\n")
         in_ade.write(f"rxn=ade.Reaction(r\"{rxn_smile}\")\n")
         in_ade.write(f"ade.Config.G16.keywords.set_functional('{functional}')\n")
-        in_ade.write(f"ade.Config.G16.keywords.opt.basis_set = '{final_basis_set}' \n")
+        in_ade.write(f"ade.Config.G16.keywords.opt.basis_set = '{basis_set}' \n")
         in_ade.write("ade.Config.G16.keywords.opt.append('tight') \n")
-        in_ade.write(f"ade.Config.G16.keywords.sp.basis_set = '{final_basis_set}' \n")
-        in_ade.write(f"ade.Config.G16.keywords.opt_ts.basis_set = '{final_basis_set}' \n")
-        in_ade.write(f"ade.Config.G16.keywords.hess.basis_set = '{final_basis_set}' \n")
-        in_ade.write(f"ade.Config.G16.keywords.low_opt.basis_set = '{initial_basis_set}' \n")
-        in_ade.write(f"ade.Config.G16.keywords.low_opt.max_opt_cycles = 25\n")
+        #in_ade.write(f"ade.Config.G16.keywords.sp.basis_set = '{final_basis_set}' \n")
+        in_ade.write(f"ade.Config.G16.keywords.opt_ts.basis_set = '{basis_set}' \n")
+        in_ade.write(f"ade.Config.G16.keywords.hess.basis_set = '{basis_set}' \n")
+        in_ade.write(f"ade.Config.G16.keywords.low_opt.basis_set = '{conf_basis_set}' \n")
+        in_ade.write(f"ade.Config.G16.keywords.low_opt.max_opt_cycles = 20\n")
         in_ade.write(f"ade.Config.num_conformers={num_conf}\n")
         in_ade.write(f"ade.Config.rmsd_threshold={rmsd}\n")
         in_ade.write(f"ade.Config.hmethod_conformers={hmet_confor}\n")
         in_ade.write('rxn.calculate_reaction_profile(free_energy=True)\n')
-        in_ade.write('t1 = time.time()\n')
-        in_ade.write('print(f"Duration: {t1-t0}")\n')
 
     return None
 
@@ -111,8 +106,8 @@ def create_slurm(idx, dir, conda_env):
     cpus_per_task = 24
     log_level = 'INFO' # {DEBUG, INFO, WARNING, ERROR}
 
-    file_name = f"slurm_{idx:05}.sh"
-    ade_idx = f"{idx:05}"
+    file_name = f"slurm_{idx}.sh"
+    ade_idx = f"{idx}"
 
     with open(f"{dir}/{file_name}", 'w') as in_slurm:
         in_slurm.write('#!/bin/bash\n')
@@ -143,4 +138,4 @@ def create_slurm(idx, dir, conda_env):
 if __name__ == "__main__":
     # set up
     args = parser.parse_args()
-    create_input_autodE(args.final_dir, args.csv_file, args.conda_env)
+    create_input_autodE(args.final_dir, args.input, args.conda_env)
